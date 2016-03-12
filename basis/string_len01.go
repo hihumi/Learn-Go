8aec0b9c7ff8cb21af2bad20d59aa41a6864b4a4
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-03-12 17:40:09 +0900

    fix

--- basis/string_len01.go
+++ basis/string_len01.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	var en string = "japanese"
 	fmt.Println(en)
-	fmt.Println(en, "len : ", len(en))
+	fmt.Println(en, "len :", len(en))
 
 	var ja string = "日本・日本語・日本人"
 	fmt.Println(ja)
-	fmt.Println(ja, "len : ", len(ja))
+	fmt.Println(ja, "len :", len(ja))
 }
