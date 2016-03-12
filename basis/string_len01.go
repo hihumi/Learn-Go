a37315c0843d7a48a0b65b41164eb4ecc3408b4f
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-03-12 17:26:15 +0900

    string len 01

---
+++ basis/string_len01.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var en string = "japanese"
+	fmt.Println(en)
+	fmt.Println(en, "len : ", len(en))
+
+	var ja string = "日本・日本語・日本人"
+	fmt.Println(ja)
+	fmt.Println(ja, "len : ", len(ja))
+}
