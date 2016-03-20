806441f944f7b448b269b415633dd26a704ba5da
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-03-21 01:32:08 +0900

    fix

--- practice/scanf02.go
+++ practice/scanf02.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var n int
 
-	fmt.Printf("This program can multiply your number by 100.\nPlease Enter a numeric number.\n>> ")
+	fmt.Printf("This program can multiply your enter integer by 100.\nPlease Enter a integer.\n>> ")
 	fmt.Scanf("%d", &n)
 
 	fmt.Printf("%d * 100 = %d\n", n, n*100)
