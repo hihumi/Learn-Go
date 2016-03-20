deb9cf24dbc089157352d591277eff82723efeda
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-03-21 01:33:14 +0900

    fix

--- practice/scanf01.go
+++ practice/scanf01.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	var n int
 
-	fmt.Printf("Please Enter a numeric number.\n>> ")
+	fmt.Printf("Please Enter a integer.\n>> ")
 
 	fmt.Scanf("%d", &n)
 
-	fmt.Printf("Number is %d\n", n)
+	fmt.Printf("Integer is %d\n", n)
 }
